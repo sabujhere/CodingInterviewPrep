c5f4ff6c1904e646d0d6e6b090ce152bcc749721
no_Sp00n <sabujece@gmail.com>
2022-02-19 22:24:10 -0600

    added RemoveAllOccurrencesOfASubstring

---
+++ Test/MiscCodingProblemsTest/FibonacciNumberTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiscCodingProblems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscCodingProblemsTest
+{
+    [TestClass]
+    public class FibonacciNumberTest
+    {
+        [TestMethod]
+        public void GetUsingRecursion_ReturnsValue()
+        {
+            var fibGenerator = new FibonacciNumber();
+            var position = 10;
+            var expectedNumber = 55;
+
+            var actualResult = fibGenerator.GetUsingRecursion(position);
+
+            Assert.AreEqual(expectedNumber, actualResult);
+        }
+
+        [TestMethod]
+        public void GetUsingIteration_ReturnsValue()
+        {
+            var fibGenerator = new FibonacciNumber();
+            var position = 10;
+            var expectedNumber = 55;
+
+            var actualResult = fibGenerator.GetUsingIteration(position);
+
+            Assert.AreEqual(expectedNumber, actualResult);
+        }
+    }
+}
---
+++ Test/MiscCodingProblemsTest/RemoveAllOccurrencesOfASubstringTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiscCodingProblems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscCodingProblemsTest
+{
+    [TestClass]
+    public class RemoveAllOccurrencesOfASubstringTest
+    {
+        [TestMethod]
+        public void Remove_ThreeCharPart_ReturnValue()
+        {
+            var strRemover = new RemoveAllOccurrencesOfASubstring();
+            var s = "daabcbaabcbc";
+            var part = "abc";
+            var expectedResult = "dab";
+            var actualResult = strRemover.Remove(s, part);
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+        [TestMethod]
+        public void Remove_TwoCharPart_ReturnValue()
+        {
+            var strRemover = new RemoveAllOccurrencesOfASubstring();
+            var s = "axxxxyyyyb";
+            var part = "xy";
+            var expectedResult = "ab";
+            var actualResult = strRemover.Remove(s, part);
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+    }
+}
