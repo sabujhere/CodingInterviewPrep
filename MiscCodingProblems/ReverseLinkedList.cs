1194f14f097deb7706f84074792825d9ed0f34dc
no_Sp00n <sabujece@gmail.com>
2022-02-25 21:06:17 -0600

    Reverse Linked List

---
+++ MiscCodingProblems/ReverseLinkedList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscCodingProblems
+{
+    /// <summary>
+    /// 206. Reverse Linked List
+    /// </summary>
+    public class ReverseLinkedList
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode node;
+            ListNode prev = null;
+            while (head != null)
+            {
+                node = head.next;
+                head.next = prev;
+                prev = head;
+                head = node;
+            }
+            return prev;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
