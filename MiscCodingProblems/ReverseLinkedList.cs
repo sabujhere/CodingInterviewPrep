879d86bd7af7ff96fdbf307befc2a4a97f5d8eba
no_Sp00n <sabujece@gmail.com>
2022-03-01 04:24:15 -0600

    590. N-ary Tree Postorder Traversal

--- MiscCodingProblems/ReverseLinkedList.cs
+++ MiscCodingProblems/ReverseLinkedList.cs
@@ -24,15 +24,4 @@ namespace MiscCodingProblems
             return prev;
         }
     }
-
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
-    }
 }
---
+++ MiscCodingProblems/models/TreeNode.cs
@@ -0,0 +1,20 @@
+﻿namespace MiscCodingProblems;
+
+public class TreeNode
+{
+    public int val;
+    public IList<TreeNode> children;
+
+    public TreeNode() { }
+
+    public TreeNode(int _val)
+    {
+        val = _val;
+    }
+
+    public TreeNode(int _val, IList<TreeNode> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+}
