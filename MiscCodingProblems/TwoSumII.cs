e9ecea55bc2edf072f95cd957a43e92ad1edb296
no_Sp00n <sabujece@gmail.com>
2022-02-28 19:13:06 -0600

    167. Two Sum II - Input Array Is Sorted

---
+++ MiscCodingProblems/TwoSumII.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscCodingProblems
+{
+    public class TwoSumII
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            var start = 0;
+            var end = numbers.Length - 1;
+
+            while(start < end)
+            {
+                var sum = numbers[start] + numbers[end];
+                if(sum > target)
+                {
+                    end--;
+                }
+                else if(sum < target)
+                {
+                    start++;
+                }
+                else
+                {
+                    return new int[] { start+1, end+1 };
+                }
+            }
+
+            return new int[] { start + 1, end + 1 };
+        }
+    }
+}
