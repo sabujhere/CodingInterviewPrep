6adef23036e39f25a90965101b94927d3c8e02ca
no_Sp00n <sabujece@gmail.com>
2022-03-07 16:47:00 -0600

    226. Invert Binary Tree

---
+++ MiscCodingProblems/InvertBinaryTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscCodingProblems
+{
+    /// <summary>
+    /// 226. Invert Binary Tree
+    /// </summary>
+    public class InvertBinaryTree
+    {
+        public InorderTreeNode InvertTree(InorderTreeNode root)
+        {
+            if (root == null) return root;
+
+            var left = InvertTree(root.left);
+            var right = InvertTree(root.right);
+            //swap
+            root.right = left;
+            root.left = right;
+            return root;
+        }
+    }
+}
