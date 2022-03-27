73c2c8645aec9527b88e158e159930b9e757a37e
no_Sp00n <sabujece@gmail.com>
2022-03-26 20:04:37 -0500

    844. Backspace String Compare

---
+++ MiscCodingProblems/BackspaceStringCompare.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscCodingProblems
+{
+    public class BackspaceStringCompare
+    {
+        /// <summary>
+        /// 844. Backspace String Compare
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public bool GetResult(string s, string t)
+        {
+            var sPointer = s.Length - 1;
+            var tPointer = t.Length - 1;
+            var sSkips = 0;
+            var tSkips = 0;
+            while (sPointer >= 0 || tPointer >= 0)
+            {
+                while (sPointer >= 0)
+                {
+                    var currentChar = s[sPointer];
+                    if (currentChar == '#')
+                    {
+                        sSkips++;
+                        sPointer--;
+                    }
+                    else if (sSkips > 0)
+                    {
+                        sPointer--;
+                        sSkips--;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                while (tPointer >= 0)
+                {
+                    var currentChar = t[tPointer];
+                    if (currentChar == '#')
+                    {
+                        tSkips++;
+                        tPointer--;
+                    }
+                    else if (tSkips > 0)
+                    {
+                        tPointer--;
+                        tSkips--;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (sPointer >= 0 && tPointer >= 0 && s[sPointer] != t[tPointer] )
+                {
+                    return false;
+                }
+
+                if ((sPointer >= 0) != (tPointer>=0))
+                {
+                    return false;
+                }
+
+                sPointer--;
+                tPointer--;
+            }
+            
+            return true;
+        }
+    }
+}
---
+++ MiscCodingProblemsTest/BackspaceStringCompareTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiscCodingProblems;
+
+namespace MiscCodingProblemsTest
+{
+    [TestClass]
+    public class BackspaceStringCompareTest
+    {
+        [TestMethod]
+        public void StringMatchTest()
+        {
+            var comparer = new BackspaceStringCompare();
+            var actualResult = comparer.GetResult("ab##", "c#d#");
+            Assert.AreEqual(actualResult, true);
+        }
+    }
+}
