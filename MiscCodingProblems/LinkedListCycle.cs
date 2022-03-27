b11ebb28941cc79d3c6c1658ca1f275c285a7430
no_Sp00n <sabujece@gmail.com>
2022-03-27 16:44:08 -0500

    141. Linked List Cycle

---
+++ MiscCodingProblems/LinkedListCycle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscCodingProblems
+{
+    /// <summary>
+    /// 141. Linked List Cycle
+    /// </summary>
+    public class LinkedListCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null) return false;
+
+            var slow = head;
+            var fast = head.next;
+
+            while (slow != fast)
+            {
+                if (fast == null || fast.next == null) return false;
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return true;
+        }
+    }
+}
