b863bf88614482676d57acb24e1adbdb51c72bc4
no_Sp00n <sabujece@gmail.com>
2022-03-27 17:20:04 -0500

    125. Valid Palindrome

---
+++ MiscCodingProblems/ValidPalindrome.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscCodingProblems
+{
+    public class ValidPalindrome
+    {
+        /// <summary>
+        /// 125. Valid Palindrome
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool IsPalindrome(string s)
+        {
+            var fixedString = new StringBuilder();
+            foreach (var c in s.ToCharArray())
+            {
+                if (char.IsLetterOrDigit(c))
+                {
+                    fixedString.Append(char.ToUpper(c));
+                }
+            }
+
+            var startPointer = 0;
+            var endPointer = fixedString.Length -1;
+            while (startPointer < endPointer)
+            {
+                if (fixedString[startPointer] != fixedString[endPointer]) return false;
+
+                startPointer++;
+                endPointer--;
+            }
+            return true;
+        }
+    }
+}
