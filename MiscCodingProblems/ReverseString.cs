851cf89b4471e42e0e072130f6096b0e7fbdae33
no_Sp00n <sabujece@gmail.com>
2022-03-27 16:57:05 -0500

    141. Linked List Cycle

---
+++ MiscCodingProblems/ReverseString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscCodingProblems
+{
+    /// <summary>
+    /// 141. Linked List Cycle
+    /// </summary>
+    public class ReverseString
+    {
+        public void Reverse(char[] s)
+        {
+            var start = 0;
+            var end = s.Length - 1;
+            while (start < end)
+            {
+                (s[start], s[end]) = (s[end], s[start]);
+                start++;
+                end--;
+            }
+        }
+    }
+}
