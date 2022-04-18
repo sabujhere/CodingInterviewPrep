5387340167408d9f5124c01dd47784fefc84aaba
no_Sp00n <sabujece@gmail.com>
2022-04-17 22:24:11 -0500

    MiddleoftheLinkedList

---
+++ MiscCodingProblems/MiddleoftheLinkedList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscCodingProblems
+{
+    public class MiddleoftheLinkedList
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            var slowPtr = head;
+            var fastPtr = head;
+            while (fastPtr != null && fastPtr.next != null)
+            {
+                slowPtr = slowPtr.next;
+                fastPtr = fastPtr.next.next;
+            }
+
+            return slowPtr;
+
+        }
+    }
+}
