45b70712283dd70df5492bbb2ca5556550b1a4c4
no_Sp00n <sabujece@gmail.com>
2022-04-18 23:04:14 -0500

    977. Squares of a Sorted Array

---
+++ MiscCodingProblems/SquaresOfASortedArray.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscCodingProblems
+{
+    public class SquaresOfASortedArray
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            if (nums.Length <= 0) throw new ArgumentException();
+            var sortedSquares = new int[nums.Length];// new List<int>(nums.Length);
+            var posPointer = 0;
+            
+            while (nums[posPointer] < 0)
+            {
+                posPointer++;
+            }
+            var negativePointer = posPointer - 1;
+            var count = 0;
+            while (negativePointer >= 0 && posPointer < nums.Length)
+            {
+                if (nums[negativePointer] * nums[negativePointer] < nums[posPointer] * nums[posPointer])
+                {
+                    sortedSquares[count] = nums[negativePointer] * nums[negativePointer];
+                    negativePointer --;
+                }
+                else
+                {
+                    sortedSquares[count] = nums[posPointer] * nums[posPointer];
+                    posPointer++;
+                }
+
+                count++;
+            }
+
+            while (negativePointer > 0)
+            {
+                sortedSquares[count] = nums[negativePointer] * nums[negativePointer];
+                negativePointer--;
+                count++;
+            }
+            while (posPointer < nums.Length)
+            {
+                sortedSquares[count] = nums[posPointer] * nums[posPointer];
+                posPointer++;
+                count++;
+            }
+
+            return sortedSquares.ToArray();
+        }
+    }
+}
