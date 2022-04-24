8cee4960888416d6d5dea63992a1fbe19e30e6d9
no_Sp00n <sabujece@gmail.com>
2022-04-23 22:58:28 -0500

    53. Maximum Subarray

---
+++ MiscCodingProblems/MaximumSubarray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscCodingProblems
+{
+    /// <summary>
+    /// 53. Maximum Subarray
+    /// </summary>
+    public class MaximumSubarray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            var currentSum = 0;
+            var maxSum = nums[0];
+            foreach (var num in nums)
+            {
+
+                if (currentSum < 0) currentSum = 0;
+                currentSum = currentSum + num;
+                maxSum = Math.Max(currentSum, maxSum);
+            }
+            return maxSum;
+
+
+        }
+    }
+}
