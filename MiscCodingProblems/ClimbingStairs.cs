547bd0d9bcb6a294aa76b379bf67dbe8dd3e78af
no_Sp00n <sabujece@gmail.com>
2022-04-24 10:28:04 -0500

    70. Climbing Stairs

---
+++ MiscCodingProblems/ClimbingStairs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscCodingProblems
+{
+    /// <summary>
+    /// 70. Climbing Stairs
+    /// </summary>
+    public class ClimbingStairs
+    {
+        private Dictionary<int, int> combinatiosByStepNumber = new Dictionary<int, int>();
+        public int GetClimbCombinations(int n)
+        {
+            return GetClimbCombinationsHelper(n);
+        }
+
+        private int GetClimbCombinationsHelper(int i)
+        {
+            int result = 0;
+            if (combinatiosByStepNumber.ContainsKey(i)) return combinatiosByStepNumber[i];
+            if (i == 1) return 1;
+            if(i == 2) return 2;
+            result = GetClimbCombinationsHelper(i - 1) + GetClimbCombinationsHelper(i - 2);
+            combinatiosByStepNumber.Add(i, result);
+            return result;
+
+        }
+    }
+}
