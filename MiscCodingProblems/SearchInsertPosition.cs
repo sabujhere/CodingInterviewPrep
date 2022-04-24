c569fa50e1169ff7038484fb9fe61c3f6191e841
no_Sp00n <sabujece@gmail.com>
2022-04-23 21:38:50 -0500

    35. Search Insert Position

---
+++ MiscCodingProblems/SearchInsertPosition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscCodingProblems
+{
+    /// <summary>
+    /// 35. Search Insert Position
+    /// </summary>
+    public class SearchInsertPosition
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            if (!nums.Any() || target < nums[0]) return 0;
+            if (target > nums.Last()) return nums.Length;
+            var start = 0;
+            var end = nums.Length - 1;
+            while (start <= end)
+            {
+                var mid = start + (end- start) / 2;
+                if (nums[mid] == target) return mid;
+                else if (nums[mid] < target) start = mid + 1;
+                else end = mid - 1;
+
+            }
+
+            return start;
+        }
+    }
+}
